9cc5958b3610d353316d08eb8ebdc6be976be9a6
Vicente Santacoloma <vsantacoloma@gmail.com>
2016-07-16 14:28:41 +0100

    Refactor Programming Exercise 2: Logistic Regression

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -2,13 +2,13 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
 %   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
-n = length(theta)
+n = length(theta);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -18,13 +18,13 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-[J, grad] = costFunction(theta, X, y)
+[J, grad] = costFunction(theta, X, y);
 
-regularized_theta = theta(2:n)
+theta(1) = 0;
 
-J = J + (lambda / (2 * m)) * (regularized_theta' * regularized_theta)
+J = J + (lambda / (2 * m)) * (theta' * theta);
 
-grad(2:n) = grad(2:n) + (lambda / m) * regularized_theta
+grad = grad + (lambda / m) * theta;
 
 % =============================================================
 
